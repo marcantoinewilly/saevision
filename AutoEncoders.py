01bdd5d21d8adcc1e484f040509fb6707e56facd
Marc-Antoine Willy <ma.willy@icloud.com>
2025-03-21 22:27:55 +0100

    Initial Commit

---
+++ AutoEncoders.py
@@ -0,0 +1,123 @@
+
+# Diverse Autoencoder Implementations in PyTorch
+
+import torch.nn as nn
+import torch
+
+class AE(nn.Module):
+    # https://arxiv.org/pdf/2502.06755 (ReLu Autoencoder)
+
+    def __init__(self, input_dim, hidden_dim):
+        super(AE, self).__init__()
+        
+        # Encoder
+        self.W_enc = nn.Linear(input_dim, hidden_dim, bias=False)
+        self.b_enc = nn.Parameter(torch.zeros(hidden_dim))
+        
+        # Decoder
+        self.W_dec = nn.Linear(hidden_dim, input_dim, bias=False)
+        self.b_dec = nn.Parameter(torch.zeros(input_dim))
+        
+        # Activation
+        self.relu = nn.ReLU()
+
+        # Loss
+        self.mse = nn.MSELoss()
+    
+    def forward(self, x):
+
+        z = self.relu(self.W_enc(x - self.b_dec) + self.b_enc)
+        xprime = self.W_dec(z) + self.b_dec
+        return x, z , xprime
+    
+    def loss(self, x, xprime):
+        return self.mse(xprime, x)
+    
+
+class SAE(nn.Module):
+    # https://arxiv.org/pdf/2502.06755 (Sparse ReLu Autoencoder)
+
+    def __init__(self, input_dim, hidden_dim):
+        super(SAE, self).__init__()
+        
+        # Encoder
+        self.W_enc = nn.Linear(input_dim, hidden_dim, bias=False)
+        self.b_enc = nn.Parameter(torch.zeros(hidden_dim))
+        
+        # Decoder
+        self.W_dec = nn.Linear(hidden_dim, input_dim, bias=False)
+        self.b_dec = nn.Parameter(torch.zeros(input_dim))
+        
+        # Activation
+        self.relu = nn.ReLU()
+
+        # Loss
+        self.mse = nn.MSELoss()
+
+        # Sparsity Loss
+        self.l1 = nn.L1Loss()
+    
+    def forward(self, x):
+
+        z = self.relu(self.W_enc(x - self.b_dec) + self.b_enc)
+        xprime = self.W_dec(z) + self.b_dec
+        return x, z , xprime
+    
+    def loss(self, x, xprime, z, alpha):
+        return self.mse(xprime, x) + alpha * self.l1(z, torch.zeros_like(z))
+    
+
+class OSAE(nn.Module):
+    # Orthogonal Sparse ReLU Autoencoder (Work in Progress)
+    
+    def __init__(self, input_dim, hidden_dim, theta, rho, gamma):
+        super(OSAE, self).__init__()
+
+        # Encoder
+        self.W_enc = nn.Linear(input_dim, hidden_dim, bias=False)
+        self.b_enc = nn.Parameter(torch.zeros(hidden_dim))
+
+        # Decoder
+        self.W_dec = nn.Linear(hidden_dim, input_dim, bias=False)
+        self.b_dec = nn.Parameter(torch.zeros(input_dim))
+
+        # Activation
+        self.relu = nn.ReLU()
+
+        # Loss
+        self.mse = nn.MSELoss()
+        
+        # Hyperparameters
+        self.theta = theta 
+        self.rho = rho  
+        self.gamma = gamma 
+
+    def forward(self, x):
+
+        z = self.relu(self.W_enc(x - self.b_dec) + self.b_enc)
+        x_hat = self.W_dec(z) + self.b_dec 
+        return x, z, x_hat
+
+    def cosine_similarity(self, z):
+
+        norm_z = torch.norm(z, p=2, dim=0, keepdim=True) + 1e-6 
+        z_normalized = z / norm_z 
+        C = torch.matmul(z_normalized.T, z_normalized)  
+        C.fill_diagonal_(0) 
+        return C
+
+    def orthogonality_penalty(self, C):
+
+        Wd = self.W_dec.weight
+        C = C.clone()
+        C.masked_fill_(C <= self.theta, 0)
+        penalty = torch.norm((Wd.T @ Wd) * C, p='fro') ** 2 
+        return penalty
+
+    def loss(self, x, x_hat, z):
+
+        mse_loss = self.mse(x_hat, x)
+        sparsity_loss = self.rho * torch.sum(torch.abs(z)) 
+        C = self.cosine_similarity(z)
+        orthogonality_loss = self.gamma * self.orthogonality_penalty(C)
+        return mse_loss + sparsity_loss + orthogonality_loss
