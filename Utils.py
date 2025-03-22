0ef060cf299c37932e15d1bbd9ae99814b987e55
Marc-Antoine Willy <ma.willy@icloud.com>
2025-03-22 21:28:57 +0100

    Fix Utils

--- Utils.py
+++ Utils.py
@@ -1,20 +1,53 @@
 import torch
 import clip
 from tqdm import tqdm
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from PIL import Image
+import os
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
 
-def getAverageViTActivation(train_loader, device, samples = 16_000, clip="ViT-B/16"):
-  
-    clip_model, clip_preprocess = clip.load(clip, device=device)
+class FlatImageDataset(Dataset):
+    def __init__(self, folder_path, resizew, resizeh, transform=None):
+        self.folder_path = folder_path
+        self.image_paths = [
+            os.path.join(folder_path, f)
+            for f in os.listdir(folder_path)
+            if f.lower().endswith((".png", ".jpg", ".jpeg"))
+        ]
+        self.transform = transform or transforms.Compose([
+            transforms.Resize((resizew, resizeh)),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        img = Image.open(img_path).convert("RGB")
+        return self.transform(img), 0
+
+
+def createImageDataset(path, resizew, resizeh):
+    return FlatImageDataset(path, resizew, resizeh)
+
+
+def createImageDataloader(path, resizew, resizeh, bsize=64, shuffle=True):
+    dataset = createImageDataset(path, resizew, resizeh)
+    dataloader = DataLoader(dataset, batch_size=bsize, shuffle=shuffle, num_workers=2)
+    return dataloader
+
+
+def getAverageViTActivation(train_loader, device, samples=16_000, clip_model_name="ViT-B/16"):
+    clip_model, clip_preprocess = clip.load(clip_model_name, device=device)
     clip_model.eval()
 
     activations = []
     total_collected = 0
-
     hooked_cls_output = {}
 
     target_block = clip_model.visual.transformer.resblocks[-2]
+
     def hook(module, input, output):
         hooked_cls_output["cls"] = output[:, 0, :]
 
@@ -23,11 +56,9 @@ def getAverageViTActivation(train_loader, device, samples = 16_000, clip="ViT-B/
     with torch.no_grad():
         for images, _ in tqdm(train_loader, desc="Collecting ViT activations"):
             images = images.to(device)
-
             images_preprocessed = torch.stack([clip_preprocess(img) for img in images]).to(device)
 
             hooked_cls_output.clear()
-
             _ = clip_model.visual(images_preprocessed)
 
             if "cls" not in hooked_cls_output:
@@ -46,25 +77,3 @@ def getAverageViTActivation(train_loader, device, samples = 16_000, clip="ViT-B/
     mean_activation = all_activations.mean(dim=0)
 
     return mean_activation
-
-def createImageDataset(path, resizew, resizeh):
-    
-    if resizew is None or resizeh is None:
-        transform = transforms.Compose([
-            transforms.ToTensor()
-        ])
-    else:
-        transform = transforms.Compose([
-            transforms.Resize((resizew, resizeh)),
-            transforms.ToTensor()
-        ])
-
-    dataset = datasets.ImageFolder(path, transform = transform)
-
-    return dataset
-
-def createImageDataloader(path, resizew, resizeh, bsize = 64, shuffle = True):
-    
-    dataset = createImageDataset(path, resizew, resizeh)
-    dataloader = DataLoader(dataset, batch_size=bsize, shuffle = shuffle, num_workers = 2)
-    return dataloader
